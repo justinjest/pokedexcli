7434156c47703c9352c3d9a2d1b59f76ce034501
justinjest <jest.cayaban@gmail.com>
2024-10-20 11:32:34 +0900

    Finished setting up basic REPL

--- main.go
+++ main.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 func main() {
+	commands := getCommands()
 	for {
 		var input string
-		fmt.Printf("pokdex > ")
+		fmt.Printf("Pokedex > ")
 		fmt.Scan(&input)
-		fmt.Printf("Input: %v \n", input)
+		command, exists := commands[input]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("Error running command.callback %v\n", err)
+			}
+		} else {
+			fmt.Printf("Sorry, that's not a valid input \n")
+		}
 	}
 }
 
@@ -17,4 +27,35 @@ type cliCommand struct {
 	name        string
 	description string
 	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
+
+func commandHelp() error {
+	commands := getCommands()
+	fmt.Printf("Welcome to the Pokedex!\n")
+	fmt.Printf("Usage:\n\n")
+	for _, cmd := range commands {
+		fmt.Printf("%v: %v \n", cmd.name, cmd.description)
+	}
+	fmt.Printf("\n")
+	return nil
+}
+
+func commandExit() error {
+	os.Exit(0)
+	return nil
 }
