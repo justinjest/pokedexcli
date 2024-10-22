90644b55319b9c31032d5b79e55a4a77fd536c77
justinjest <jest.cayaban@gmail.com>
2024-10-22 18:30:30 +0900

    Working on cache

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,20 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	cache map[string]cacheEntry
+	mu    *sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func (c Cache) NewCache() {
+
+}
